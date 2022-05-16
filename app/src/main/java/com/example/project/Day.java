75fcb2829f9c1522f34edea7bdb54e09419486c0
Erik Holmberg <b21eriho@student.his.se>
2022-05-16 20:32:41 +0200

    Implement first draft of day-class

---
+++ app/src/main/java/com/example/project/Day.java
@@ -0,0 +1,72 @@
+package com.example.project;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Day {
+    @SerializedName("size")
+    private int date;
+    private String name;
+    @SerializedName("location")
+    private String sunUp;
+    @SerializedName("company")
+    private String sunDown;
+    @SerializedName("auxdata")
+    private List<String> events;
+
+    public int getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSunUp() {
+        return sunUp;
+    }
+
+    public String getSunDown() {
+        return sunDown;
+    }
+
+    public List<String> getEvents() {
+        return events;
+    }
+
+    public String getSunTimesShort(){
+        String[] sunUpShort = sunUp.split(":");
+        String[] sunDownShort = sunDown.split(":");
+
+        String returner = sunUpShort + "-" + sunDownShort;
+
+        return returner;
+    }
+
+    @Override
+    public String toString() {
+        String returner = "";
+
+        returner += date + "";
+
+        switch (date){
+            case 1:
+                returner += "st ";
+                break;
+            case 2:
+                returner += "nd ";
+                break;
+            case 3:
+                returner += "rd ";
+                break;
+            default:
+                returner += "th ";
+                break;
+        }
+
+        returner += "of Lightrise";
+
+        return returner;
+    }
+}
